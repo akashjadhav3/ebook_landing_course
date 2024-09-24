d7398f2be42e008aef50f893de2deeb135aaf12c
Nik <niklas@kizo-agency.com>
2024-09-24 20:22:16 +0200

    1_9 chapter preview component styling

--- src/lib/components/ChapterPreview.svelte
+++ src/lib/components/ChapterPreview.svelte
@@ -4,7 +4,7 @@
     <ul>
       <li>
         <button
-          class="chapter-title"
+          class="chapter-title selected-chapter-title"
           aria-controls="chapter-info-1"
           aria-expanded="true"
         >
@@ -31,3 +31,42 @@
     </div>
   </div>
 </section>
+
+<style>
+  .chapter-preview {
+    padding: 80px 0;
+    margin-left: 12vw;
+    margin-right: 20vw;
+    max-width: 1150px;
+  }
+
+  .chapter-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .chapter-container ul {
+    width: 40%;
+  }
+
+  .chapter-info {
+    width: 55%;
+  }
+
+  .chapter-title {
+    border-bottom: 1px solid grey;
+    width: 100%;
+    display: block;
+    padding: 12px;
+    text-align: left;
+  }
+
+  .selected-chapter-title {
+    background-color: black;
+    border: none;
+    color: white;
+    box-shadow:
+      0 4px 6px rgba(0, 0, 0, 0.1),
+      0 1px 3px rgba(0, 0, 0, 0.08);
+  }
+</style>
