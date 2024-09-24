dd90f9bbdf6852869321dd5165a494c8fc58792b
Nik <niklas@kizo-agency.com>
2024-09-24 21:32:39 +0200

    1_16 - Faq section styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script>
   // import HeroSection from "../lib/components/HeroSection.svelte";
-  import { HeroSection, ChapterPreview, AuthorSection } from "$components";
+  import {
+    HeroSection,
+    ChapterPreview,
+    AuthorSection,
+    FaqSection,
+  } from "$components";
 </script>
 
 <HeroSection />
 <ChapterPreview />
 <AuthorSection />
+<FaqSection />
