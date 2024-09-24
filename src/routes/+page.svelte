bdfe0d382721d17c7dd011ac2570defa9c3d572e
Nik <niklas@kizo-agency.com>
2024-09-24 14:38:35 +0200

    1_8 - chapter preview basic html structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   // import HeroSection from "../lib/components/HeroSection.svelte";
-  import { HeroSection } from "$components";
+  import { HeroSection, ChapterPreview } from "$components";
 </script>
 
 <HeroSection />
+<ChapterPreview />
